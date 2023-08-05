f5ddc2416b6aa8f5d802698eb586f28496c9ec63
Jorge Gimenez <jorge.2015.8893@gmail.com>
2023-08-05 00:12:13 -0300

    Nueva configuración
    No se ha podido iniciar debidamente el servidor, por lo tanto, es necesario reiniciar algunos procesos.

---
+++ 2022-sep-Equipo8-main/informatorio/b_final.sql
@@ -0,0 +1,2 @@
+create database b_final;
+use b_final;
---
+++ 2022-sep-Equipo8-main/informatorio/configuraciones/local.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'b_final',
+        'USER': 'root',
+        'PASSWORD': 'rw-22.93-jg',
+        'HOST': 'localhost',
+        'PORT': 3306,
+    }
+}
